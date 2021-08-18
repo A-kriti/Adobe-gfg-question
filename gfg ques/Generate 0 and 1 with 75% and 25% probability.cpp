69791f127010a2baa8520c5a730041c454344e67
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-08-18 22:18:40 +0530

    Create Generate 0 and 1 with 75% and 25% probability.cpp

---
+++ gfg ques/Generate 0 and 1 with 75% and 25% probability.cpp
@@ -0,0 +1,35 @@
+// fuction to generate 0 and 1 with 75% and 25% probability
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int rand25()
+{
+    
+    return rand()%2 & rand()%2;
+}
+
+
+
+int main()
+{
+    
+    int n=50000;
+    int c0=0;
+    int c1=0;
+    for(int i=0;i<n;i++){
+    
+        //cout<<rand75()<<" ";
+        if(rand25()==0){
+            c0++;
+        }
+        else{
+            c1++;
+        }
+        
+    }
+    cout<<"\n";
+    cout<<"c0 is "<<((c0*100)/n)<<endl;
+    cout<<"c1 is "<<((c1*100)/n);
+    return 0;
+}

7d4d58e761396b6d9b57fd1ae2bb69bd633e5791
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-09-27 18:53:53 +0530

    Create 0 - 1 Knapsack Problem.cpp

---
+++ Prep/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,40 @@
+// ques - https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+
+class Solution
+{
+    public:
+    
+    
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+     
+     //int N=n;
+     int t[n+1][W+1];
+     
+     for(int i=0;i<=n;i++){
+         for(int j=0;j<=W;j++){
+             
+             if(i==0 || j==0){
+                 t[i][j]=0;
+             }
+             
+             else{
+                 
+                 if(wt[i-1]<=j){
+                     
+                     t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+                 }
+                 
+                 else{
+                     t[i][j]=t[i-1][j];
+                 }   
+                 
+             }
+             
+     }
+     }
+     
+     return t[n][W];
+     
+    }
+};
